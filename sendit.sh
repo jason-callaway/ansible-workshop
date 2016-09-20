a7de6ebad8fa92151c55b4d8e20171abd95ad1ad
Jason Callaway <jason@jasoncallaway.com>
2016-09-19 20:41:37 -0400

    tweaks for workshop tomorrow

--- sendit.sh
+++ sendit.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 export ANSIBLE_CONFIG=./ansible.cfg
+ansible-playbook -i "localhost" localhost.yml && \
 ansible-playbook -i inventory/aws/hosts/ec2.py bastion.yml && \
 # After we're done with the dynamic part, we switch to the static
 # hosts file and the Ansible config which tunnels through the bastion
